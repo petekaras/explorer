0d4ced344b5d7295c5b934a2160e7112218def9c
Peter Karas <peter@peterkaras.com>
2015-06-02 22:45:27 +0100

    Add in a table to display results of search
    Link to static data. No endppoint for this yet.

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,6 +1,32 @@
-var app = angular.module('app', [ 'ui.bootstrap' ]);
+var app = angular.module('app', [ 'ui.bootstrap','ngTable' ]);
 
-app.controller('appController', function($scope, DataService) {
+app.controller('appController', function($scope, DataService, ngTableParams) {
+    
+    $scope.libraries = [{groupId: "org.springframework", artifactId: "spring-core",version:"4.0.4.RELEASE"},
+                    {groupId: "merlin", artifactId: "merlin-api",version:"3.3.0"},
+                    {groupId: "org.apache.camel", artifactId: "camel",version:"2.15.2"},
+                    {groupId: "com.fizz-buzz", artifactId: "fb-android-dagger",version:"1.0.6"},
+                    {groupId: "org.gridkit.jvmtool", artifactId: "hprof-heap",version:"0.3.6"}];
+    
+    
+    
+    $scope.tableParams = new ngTableParams({
+        page: 1,            // show first page
+        count: 10           // count per page
+    }, {
+        total: $scope.libraries.length, // length of data
+        getData: function ($defer, params) {
+            var pageData = $scope.libraries.slice((params.page() - 1) * params.count(), params.page() * params.count()),
+                sum = 0;
+
+ 
+            $defer.resolve(pageData);
+        }
+    })
+    
+    
+    
+    
     
     
     $scope.getMavenData = function(form) {		
@@ -13,6 +39,17 @@ app.controller('appController', function($scope, DataService) {
 	    });
 	}
     };
+    
+    $scope.getMavenDataFromTableClick = function(library) {		
+
+
+	    DataService.getData($scope, {
+		'groupId' : library.groupId,
+		'artifactId' : library.artifactId,
+		'version' : library.version
+	    });
+
+    };    
 
 });
 
